74f920483a5b0ff5bd81120c23d4855934732415
Ryoh Kawai <kawairyo@rg-automated.jp>
2022-07-22 11:05:27 +0000

    Rename

--- bin/cdk-standard-stack.ts
+++ bin/cdk-standard-stack.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkTemplateTypescriptStack } from '../lib/cdk-template-typescript-stack';
+import { CdkStandardStackStack } from '../lib/cdk-standard-stack-stack';
 
 const app = new cdk.App();
-new CdkTemplateTypescriptStack(app, 'CdkTemplateTypescriptStack', {
+new CdkStandardStackStack(app, 'CdkStandardStackStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- test/cdk-standard-stack.test.ts
+++ test/cdk-standard-stack.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkTemplateTypescript from '../lib/cdk-template-typescript-stack';
+// import * as CdkStandardStack from '../lib/cdk-standard-stack-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-template-typescript-stack.ts
+// example resource in lib/cdk-standard-stack-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new CdkTemplateTypescript.CdkTemplateTypescriptStack(app, 'MyTestStack');
+//   const stack = new CdkStandardStack.CdkStandardStackStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
