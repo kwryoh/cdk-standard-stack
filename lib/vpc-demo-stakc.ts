c8d58d92193f4ede1a40aed3c468b3c1e845a39a
Ryoh Kawai <kawairyo@rg-automated.jp>
2022-07-25 03:15:10 +0000

    Change Retab

--- lib/vpc-demo-stakc.ts
+++ lib/vpc-demo-stakc.ts
@@ -3,20 +3,20 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class VpcDemoStack extends Stack {
-    public readonly vpc: ec2.Vpc;
+  public readonly vpc: ec2.Vpc;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        this.vpc = new ec2.Vpc(this, 'CdkDemoVpc', {
-            cidr: "10.0.0.0/16",
-            subnetConfiguration: [
-                {
-                    cidrMask: 24,
-                    name: "public",
-                    subnetType: ec2.SubnetType.PUBLIC
-                },
-            ]
-        });
-    }
+    this.vpc = new ec2.Vpc(this, 'CdkDemoVpc', {
+      cidr: "10.0.0.0/16",
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public",
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+      ]
+    });
+  }
 }
