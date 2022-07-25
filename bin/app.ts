a6ec729a3d071a973a6d4b7b7f228abbdae43bce
Ryoh Kawai <kawairyo@rg-automated.jp>
2022-07-25 09:00:02 +0000

    Fix propsを共通化 Fix Typo

--- bin/app.ts
+++ bin/app.ts
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { VpcDemoStack } from '../lib/vpc-demo-stakc';
+import { VpcDemoStack } from '../lib/vpc-demo-stack';
 import { Ec2DemoStack } from '../lib/ec2-demo-stack';
 
 const app = new cdk.App();
-const vpc = new VpcDemoStack(app, 'VpcDemoStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-});
+const defaultProps = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  }
+};
+const network = new VpcDemoStack(app, 'VpcDemoStack', defaultProps);
 
 new Ec2DemoStack(app, 'Ec2DemoStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  vpc: vpc.vpc,
+  ...defaultProps,
+  vpc: network.vpc,
   keyname: 'rkw_home',
 });
