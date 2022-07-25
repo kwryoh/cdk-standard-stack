b60083f0d99201d70838f2950c6ebb3649cab2a1
Ryoh Kawai <kawairyo@rg-automated.jp>
2022-07-25 12:34:25 +0000

    Add comments

--- lib/ec2-demo-stack.ts
+++ lib/ec2-demo-stack.ts
@@ -11,15 +11,21 @@ export class Ec2DemoStack extends Stack {
   constructor(scope: Construct, id: string, props: Ec2DemoStackProps) {
     super(scope, id, props);
 
+    // EC2インスタンスの生成
     const instance = new ec2.Instance(this, 'CdkDemoInstance', {
+      // VPC ID
       vpc: props.vpc,
+      // インスタンス種別（t2-micro）
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      // AMI (AmazonLinux 2 kernel 5)
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
         kernel: ec2.AmazonLinuxKernel.KERNEL5_X,
       }),
+      // SSH キーペア
       keyName: props.keyname,
     });
+    // SecurityGroup
     instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22));
     instance.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
   }
